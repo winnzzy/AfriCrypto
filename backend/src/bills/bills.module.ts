6794c6c90646ef4dd8d90bbe4b109002be79faa3
Winner Nnamuah <nnamuahn@gmail.com>
2026-09-18 19:15:45 +0100

    Register pluggable bill provider

--- backend/src/bills/bills.module.ts
+++ backend/src/bills/bills.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BillsService } from './bills.service';
 import { BillsController } from './bills.controller';
 import { TransactionsModule } from '../transactions/transactions.module';
+import { BillProvider } from './providers/bill-provider';
+import { SandboxBillProvider } from './providers/sandbox-bill-provider';
 
 @Module({
-  imports: [TransactionsModule],
-  providers: [BillsService],
+  imports: [TransactionsModule, ConfigModule],
+  providers: [BillsService, SandboxBillProvider, { provide: BillProvider, useExisting: SandboxBillProvider }],
   controllers: [BillsController],
 })
 export class BillsModule {}
